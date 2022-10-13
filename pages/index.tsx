467ef3df2ced899ff7ac6f0249c8fccb20d23233
Johannes W <johannes@trilo.io>
2022-10-13 15:30:29 +0100

    adding cleanChaosLevel formatting util LOLz

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,26 @@ import Header from "../src/components/Header";
 import Roulette from "../src/components/Roulette";
 import SEO from "../src/components/SEO";
 
+const defaultChaosLevel = 5;
+const invalidChaosLevel = 11;
+const validChaosLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function cleanChaosLevel(c: string) {
+  if (!c) {
+    return defaultChaosLevel;
+  }
+
+  let chaosLevel = parseInt(c);
+  if (!validChaosLevels.includes(chaosLevel)) {
+    return invalidChaosLevel;
+  }
+
+  return chaosLevel;
+}
+
 const Home: NextPage = () => {
   const { query } = useRouter();
-  const chaosLevel = +(query.c as string);
+  const chaosLevel = cleanChaosLevel(query.c as string);
 
   return (
     <>
