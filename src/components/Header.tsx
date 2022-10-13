5deb20915da80a6bd45df6844e3033ee1b1ed748
Johannes W <johannes@trilo.io>
2022-10-13 15:54:06 +0100

    adding chaosLevel buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,50 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { FC } from "react";
 
 type Props = {
   chaosLevel: number;
 };
 
-const Header: FC<Props> = ({ chaosLevel }) => (
-  <div className="flex flex-col items-center w-screen text-cyan-500 font-bold text-4xl mt-2">
-    <div className="flex gap-2">
-      <span>Chaos level: </span>
-      <span className="animate-bounce">{chaosLevel}</span>
-    </div>
-    {chaosLevel > 8 && (
-      <div>
-        <br />
-        <Image src="/ahh.gif" alt="AHHHH" width={300} height={300} />
+const Header: FC<Props> = ({ chaosLevel }) => {
+  const { push } = useRouter();
+
+  return (
+    <div className="flex flex-col items-center w-screen text-cyan-500 font-bold text-4xl mt-2">
+      <div className="flex gap-2">
+        <span>Chaos level: </span>
+        <span className="animate-bounce w-12">{chaosLevel}</span>
+        <button
+          style={{ opacity: chaosLevel === 11 ? 0.2 : 1 }}
+          disabled={chaosLevel === 11}
+          onClick={() =>
+            push({ pathname: "/", query: { c: chaosLevel + 1 } }, undefined, {
+              shallow: true,
+            })
+          }
+        >
+          ⬆️
+        </button>
+        <button
+          style={{ opacity: chaosLevel === 1 ? 0.2 : 1 }}
+          disabled={chaosLevel === 1}
+          onClick={() =>
+            push({ pathname: "/", query: { c: chaosLevel - 1 } }, undefined, {
+              shallow: true,
+            })
+          }
+        >
+          ⬇️
+        </button>
       </div>
-    )}
-  </div>
-);
+      {chaosLevel > 8 && (
+        <div>
+          <br />
+          <Image src="/ahh.gif" alt="AHHHH" width={300} height={300} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Header;
