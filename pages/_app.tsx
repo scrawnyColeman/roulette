0b010be6988f747e9588e7b90a05672e55cbb11a
Johannes W <johannes@trilo.io>
2022-10-13 15:45:07 +0100

    moving chaosLevel to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+const defaultChaosLevel = 5;
+const invalidChaosLevel = 11;
+const validChaosLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function cleanChaosLevel(c: string) {
+  if (!c) {
+    return defaultChaosLevel;
+  }
+
+  let chaosLevel = parseInt(c);
+  if (!validChaosLevels.includes(chaosLevel)) {
+    return invalidChaosLevel;
+  }
+
+  return chaosLevel;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  const { query } = useRouter();
+  const chaosLevel = cleanChaosLevel(query.c as string);
+  return <Component {...pageProps} chaosLevel={chaosLevel} />;
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,14 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 
 import Header from "../src/components/Header";
 import Roulette from "../src/components/Roulette";
 import SEO from "../src/components/SEO";
 
-const defaultChaosLevel = 5;
-const invalidChaosLevel = 11;
-const validChaosLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+type Props = {
+  chaosLevel: number;
+};
 
-function cleanChaosLevel(c: string) {
-  if (!c) {
-    return defaultChaosLevel;
-  }
-
-  let chaosLevel = parseInt(c);
-  if (!validChaosLevels.includes(chaosLevel)) {
-    return invalidChaosLevel;
-  }
-
-  return chaosLevel;
-}
-
-const Home: NextPage = () => {
-  const { query } = useRouter();
-  const chaosLevel = cleanChaosLevel(query.c as string);
-
+const Home: NextPage<Props> = ({ chaosLevel }) => {
   return (
     <>
       <SEO />
